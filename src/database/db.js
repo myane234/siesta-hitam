d8f104a9ef7ab2d8b19a62d6b063e339231060f6
myane <157201609+myane234@users.noreply.github.com>
2025-12-16 20:49:23 +0700

    Delete src/database directory

--- src/database/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123',
-    database: 'bot_tele',
-    port: 3306
-})
